070b577113246fac95c3066db57a956a8088046a
bruno simon <brunosimon999@gmail.com>
2024-07-31 19:03:08 -0300

    init

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+
+const routes: Routes = [
+  {
+    path: 'login',
+    loadChildren: () => import('./components/login/login.module').then(m => m.LoginModule),
+  },
+  {
+    path: '',
+    redirectTo: 'financeiro',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: 'financeiro',
+    pathMatch: 'full'
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'login',
+        loadChildren: () => import('./components/login/login.module').then(m => m.LoginModule),
+      },
+];
